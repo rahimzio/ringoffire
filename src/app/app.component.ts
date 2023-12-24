a9ec86e1ef9f87b08d66bc92476f7acd9942a519
rahimzio <116596740+rahimzio@users.noreply.github.com>
2023-12-24 22:46:31 +0000

    first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { StartScreenComponent } from './start-screen/start-screen.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,StartScreenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
